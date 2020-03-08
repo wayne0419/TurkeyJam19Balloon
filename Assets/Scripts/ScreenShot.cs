41358217ff676838fd19fae7c09d96dcc1ff0fd7
williamdong <dzy1997@hotmail.com>
2020-03-07 20:15:49 -0500

    build 1.0
    First version to App Store

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenShot : MonoBehaviour
+{
+    public Text score;
+    public string filename = "screenshot.png";
+    bool done = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!done && score.text == "Score: 10"){
+            ScreenCapture.CaptureScreenshot(filename);
+            Debug.Log("taken screenshot");
+            done = true;
+        }
+    }
+}
